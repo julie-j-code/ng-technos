815a403872b2132b65b7509f57f1244937b3c93a
Julie J <jeannet.julie@gmail.com>
2021-02-16 15:24:14 +0100

    rectification chemin import Technolody

--- src/app/techno-details/techno-details.component.ts
+++ src/app/techno-details/techno-details.component.ts
@@ -1,5 +1,5 @@
 import { Component, Input, OnInit } from '@angular/core';
-import { Technology } from '../models/technologys';
+import { Technology } from "../models/technologys";
 
 @Component({
   selector: 'app-techno-details',
